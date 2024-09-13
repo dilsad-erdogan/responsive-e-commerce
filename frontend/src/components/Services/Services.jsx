60711c46ff6bc14ba2c851da13469acf1449bf62
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-13 11:45:43 +0300

    Services

--- frontend/src/components/Services/Services.jsx
+++ frontend/src/components/Services/Services.jsx
@@ -3,25 +3,25 @@ import { FaCarSide, FaHeadphonesAlt, FaWallet, FaCheckCircle } from "react-icons
 const ServiceData = [
     {
         id: 1,
-        icon: <FaCarSide className="text-4xl md: text-5xl text-primary"></FaCarSide>,
+        icon: <FaCarSide className="text-4xl md:text-5xl text-primary"></FaCarSide>,
         title: "Free Shipping",
         description: "Free Shipping On All Order",
     },
     {
         id: 2,
-        icon: <FaCheckCircle className="text-4xl md: text-5xl text-primary"></FaCheckCircle>,
+        icon: <FaCheckCircle className="text-4xl md:text-5xl text-primary"></FaCheckCircle>,
         title: "Safe Money",
         description: "30 Days Money Back",
     },
     {
         id: 3,
-        icon: <FaWallet className="text-4xl md: text-5xl text-primary"></FaWallet>,
+        icon: <FaWallet className="text-4xl md:text-5xl text-primary"></FaWallet>,
         title: "Secure Payment",
         description: "All Payment Secure",
     },
     {
         id: 4,
-        icon: <FaHeadphonesAlt className="text-4xl md: text-5xl text-primary"></FaHeadphonesAlt>,
+        icon: <FaHeadphonesAlt className="text-4xl md:text-5xl text-primary"></FaHeadphonesAlt>,
         title: "Online Support 24/7",
         description: "Technical Support 24/7",
     },
