b7615a6eb7993afb5f9ebef1ceea5361b435b867
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-14 12:22:32 +0300

    Banner 2 adding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,10 @@ import Category2 from "./components/Category/Category2";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
+import Products from "./components/Products/Products";
+
 import headphone from "./assets/hero/headphone.png";
+import smartwatch from "./assets/category/smartwatch2-removebg-preview.png";
 
 const BannerData = {
   discount: "30% OFF",
@@ -17,6 +20,17 @@ const BannerData = {
   bgColor: "#f42c37",
 };
 
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis.",
+  bgColor: "#2dcc6f",
+};
+
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
@@ -26,6 +40,8 @@ function App() {
       <Category2 />
       <Services />
       <Banner data={BannerData} />
+      <Products />
+      <Banner data={BannerData2} />
     </div>
   )
 }
