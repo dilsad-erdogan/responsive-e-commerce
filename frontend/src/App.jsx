79d91dde90196a6c421812a50cf7824b74929354
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-14 12:18:32 +0300

    Banner adding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import Banner from "./components/Banner/Banner";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
+import headphone from "./assets/hero/headphone.png";
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis.",
+  bgColor: "#f42c37",
+};
 
 function App() {
   return (
@@ -12,6 +25,7 @@ function App() {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
     </div>
   )
 }
